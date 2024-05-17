ae62e0db0d40214310a76747bbfb6d6426f9a68b
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2024-05-17 11:32:29 -0400

    abstract

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-class Department {
+abstract class Department {
   static fiscalYear = 2020;
   // private id: string;
   // private name: string;
   protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
   }
@@ -13,9 +13,7 @@ class Department {
     return { name: name };
   }
 
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name} `);
-  }
+  abstract describe(this: Department): void;
 
   addEmployee(employee: string) {
     this.employees.push(employee);
@@ -30,6 +28,10 @@ class Department {
 class ITDepartment extends Department {
   constructor(id: string, public admin: string[]) {
     super(id, "IT");
+  }
+
+  describe() {
+    console.log('IT Department -ID: ' + this.id);
   }
 }
 
@@ -54,6 +56,11 @@ class AccountingDepartment extends Department {
     super(id, "Accountant");
     this.lastReport = reports[0];
   }
+
+  describe() {
+    console.log('Accounting Department - ID: ' + this.id)
+  }
+
   addEmployee(name: string) {
     if (name === "Max") {
       return;
@@ -95,8 +102,9 @@ console.log(accounting.mostRecentReport);
 accounting.addEmployee("Max");
 accounting.addEmployee("Manu");
 
-accounting.printReports();
-accounting.printEmployeeInformation();
+// accounting.printReports();
+// accounting.printEmployeeInformation();
+accounting.describe();
 
 // const accountingCopy = { name: 'DUMMY', describe: it.describe };
 
