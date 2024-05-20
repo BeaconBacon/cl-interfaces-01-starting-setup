7980c4b697214baff3fce80c9557dac1d4187976
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2024-05-20 16:58:29 -0400

    interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,25 @@
-interface Person {
-    name: string;
-    age: number;
+interface Greetable {
+  name: string;
 
-    greet(phrase: string): void;
+  greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
+  age = 30;
 
-user1 = {
-    name: 'Max',
-    age: 30,
-    greet(phrase: string) {
-        console.log(phrase + ' ' + this.name);
-    }
+  constructor(n: string) {
+    this.name = n;
+  }
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  }
+}
 
-};
+let user1: Greetable;
 
-user1.greet('Hi there - I am');
+user1 = new Person('Max');
+
+user1.greet("Hi there - I am");
+
+console.log(user1);
